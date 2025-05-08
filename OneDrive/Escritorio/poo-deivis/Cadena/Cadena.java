5b1b7aec1dd359f1087de2706394f83bc4f18dfd
buxter-deivis <20243rd031@utez.edu.mx>
2025-05-08 13:40:55 -0600

    cuarto guardado

--- OneDrive/Escritorio/poo-deivis/Cadena/Cadena.java
+++ OneDrive/Escritorio/poo-deivis/Cadena/Cadena.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Cadena {
     public static void main (String [] args){
     Scanner scanner = new Scanner (System.in);
-        System.out.println("Ingresa tu nombre(s) ");
+    System.out.println("Ingresa tu nombre(s) ");
     String nombre = Scanner.nextline();
     System.out.println("Ingresa tu apellido paterno" );
     String apellidopat = Scanner.nextline();
     System.out.println("Ingresa tu apellido materno " );
     String apellidomat = Scanner.nextline();
-    System.out.println("Tu nombre completoo es = "  "+nombre+"  "+apellidopat+" "+apellidomat" );
+    System.out.println("Tu nombre completo es = " + nombre "" + apellidopat" "+ apellidomat );
 
     }
 }
