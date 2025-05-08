52b4aaa04a4042c449ecd1ab4d9e01b1f25cf8d1
buxter-deivis <20243rd031@utez.edu.mx>
2025-05-08 13:28:35 -0600

    Primer guardado

---
+++ OneDrive/Escritorio/poo-deivis/Cadena/Cadena.java
@@ -0,0 +1,8 @@
+public class Cadena {
+    public static void main (String [] args){
+    Scanner scanner = new Scanner (System.in);
+        System.out.println("Ingresa tu nombre " + nombre );
+    String nombre = Scanner.nextline();
+
+    }
+}
